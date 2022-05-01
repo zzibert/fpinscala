46647251002e06535c12639d164e534a29587966
Žan Žibert <zzibert@plume.com>
2022-05-01 18:03:54 +0200

    Exercise 2.2 : IsSorted function

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -154,7 +154,17 @@ object PolymorphicFunctions {
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+    if (as.length < 2) {
+      true
+    } else {
+      if (gt(as(1), as(0))) {
+        isSorted(as.drop(1), gt)
+      } else {
+        false
+      }
+    }
+  }
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
